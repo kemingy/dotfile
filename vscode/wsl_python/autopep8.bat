04b42227753da43cc65a26a99d34a84ce01046c9
keming <kemingy94@gmail.com>
2019-04-04 16:59:11 +0800

    add python vscode wsl settings

---
+++ vscode/wsl_python/autopep8.bat
@@ -0,0 +1,6 @@
+@echo off
+set v_params=%*
+set v_params=%v_params:\=/%
+set v_params=%v_params:c:=/mnt/c%
+set v_params=%v_params:"=\"%
+bash.exe -c "/home/keming/miniconda3/bin/autopep8 %v_params%"
---
+++ vscode/wsl_python/ctags.bat
@@ -0,0 +1,6 @@
+@echo off
+set v_params=%*
+set v_params=%v_params:\=/%
+set v_params=%v_params:c:=/mnt/c%
+set v_params=%v_params:"=\"%
+bash.exe -c "ctags %v_params%"
