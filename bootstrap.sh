012b340fde48c0205b83914811f16337d3e03f14
Keming <kemingy94@email.com>
2019-04-24 23:44:44 +0800

    change ~ to /Users/keming.yang, fix #2

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,27 +3,27 @@ set -e
 
 CONDA="Miniconda3-latest-Linux-x86_64.sh"
 
-mkdir -p ~/Downloads
-[ -f "~/Downloads/${CONDA}" ] || wget -O ~/Downloads/${CONDA} "https://repo.anaconda.com/miniconda/${CONDA}"
+mkdir -p ${HOME}/Downloads
+[ -f "${HOME}/Downloads/${CONDA}" ] || wget -O ${HOME}/Downloads/${CONDA} "https://repo.anaconda.com/miniconda/${CONDA}"
 # start | accept | location | init
-printf '\nyes\n\nyes\n' | sh ~/Downloads/${CONDA} -s -- -u
-sh ~/.bashrc
+printf '\nyes\n\nyes\n' | sh ${HOME}/Downloads/${CONDA} -s -- -u
+sh ${HOME}/.bashrc
 
 # vimrc
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-cat vim/.vimrc >> ~/.vim_runtime/my_configs.vim
+git clone --depth=1 https://github.com/amix/vimrc.git ${HOME}/.vim_runtime
+sh ${HOME}/.vim_runtime/install_awesome_vimrc.sh
+cat vim/.vimrc >> ${HOME}/.vim_runtime/my_configs.vim
 
 # Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-sh ~/.cargo/env
+sh ${HOME}/.cargo/env
 cargo install lsd --force
 cargo install bat --force
 cargo install xsv --force
 
 # process to install fish-shell
 printf '\n' | conda install -c conda-forge fish
-cat config.fish >> ~/.config/fish/config.fish
+cat config.fish >> ${HOME}/.config/fish/config.fish
 curl -L https://get.oh-my.fish | fish
 omf install bira
 
--- bootstrap_basic.sh
+++ bootstrap_basic.sh
@@ -3,15 +3,15 @@ set -e
 
 CONDA="Miniconda3-latest-Linux-x86_64.sh"
 
-mkdir -p ~/Downloads
-[ -f "~/Downloads/${CONDA}" ] || wget -O ~/Downloads/${CONDA} "https://repo.anaconda.com/miniconda/${CONDA}"
+mkdir -p ${HOME}/Downloads
+[ -f "${HOME}/Downloads/${CONDA}" ] || wget -O ${HOME}/Downloads/${CONDA} "https://repo.anaconda.com/miniconda/${CONDA}"
 # start | accept | location | init
-printf '\nyes\n\nyes\n' | sh ~/Downloads/${CONDA} -s -- -u
-sh ~/.bashrc
+printf '\nyes\n\nyes\n' | sh ${HOME}/Downloads/${CONDA} -s -- -u
+sh ${HOME}/.bashrc
 
 # vimrc
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
+git clone --depth=1 https://github.com/amix/vimrc.git ${HOME}/.vim_runtime
+sh ${HOME}/.vim_runtime/install_basic_vimrc.sh
 
 # process to install fish-shell
 printf '\n' | conda install -c conda-forge fish
