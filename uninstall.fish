ff104b80d6d162b126fb4df6128e2852541d426e
Keming <kemingy94@email.com>
2019-04-25 00:17:44 +0800

    add rm omf folder to uninstall

--- uninstall.fish
+++ uninstall.fish
@@ -1,10 +1,11 @@
 # vimrc
-rm -rf ~/.vim_runtime
+rm -rf $HOME/.vim_runtime
 
 # rust
 printf 'y\n' | rustup self uninstall
 
 # fish
-rm ~/.config/fish/config.fish
+rm $HOME/.config/fish/config.fish
+rm $HOME/.local/share/omf
 printf 'y\n' | omf destroy
 printf '\n' | conda remove fish
