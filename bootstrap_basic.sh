4592a56fc7e2248966043a9549ef6f3f73af1bc7
Keming <kemingy94@email.com>
2019-04-24 23:56:14 +0800

    change sh to .

--- bootstrap_basic.sh
+++ bootstrap_basic.sh
@@ -7,7 +7,7 @@ mkdir -p ${HOME}/Downloads
 [ -f "${HOME}/Downloads/${CONDA}" ] || wget -O ${HOME}/Downloads/${CONDA} "https://repo.anaconda.com/miniconda/${CONDA}"
 # start | accept | location | init
 printf '\nyes\n\nyes\n' | sh ${HOME}/Downloads/${CONDA} -s -- -u
-sh ${HOME}/.bashrc
+. ${HOME}/.bashrc
 
 # vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ${HOME}/.vim_runtime
