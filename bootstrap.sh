b917904bc8e1d8d36a85e87d41c2c206bdb66433
Keming <kemingy94@email.com>
2019-04-24 20:10:17 +0800

    add bootstrap sh

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+CONDA = "Miniconda3-latest-Linux-x86_64.sh"
+
+mkdir -p ~/Downloads
+cd ~/Downloads && wget "https://repo.anaconda.com/miniconda/$CONDA"
+chmod +x ~/Donwloads/CONDA
+# start | accept | location | init
+printf '\nyes\n\nyes\n' | sh ~/Downloads/CONDA
+bash
+
+# process to install fish-shell
+printf '\n' | conda install -c conda-forge fish
+curl -L https://get.oh-my.fish | fish
+omf install bira
+
+# vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+
+echo "Enjoy :-)"
