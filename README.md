7a08a947dba81b434f46d0a776bebf5135d6c4e7
Keming <keming.yang@shopee.com>
2019-04-24 20:21:06 +0800

    fix bash variable bug

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This is my personal dotfile.
 
 * mini-conda
 * fish-shell
+
+## usage
+
+```sh
+git clone --depth=1 https://github.com/kemingy/dotfile.git ~/.dotfile
+cd ~/.dotfile && sh bootstrap.sh
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-CONDA = "Miniconda3-latest-Linux-x86_64.sh"
+CONDA="Miniconda3-latest-Linux-x86_64.sh"
 
 mkdir -p ~/Downloads
-cd ~/Downloads && wget "https://repo.anaconda.com/miniconda/$CONDA"
-chmod +x ~/Donwloads/CONDA
+cd ~/Downloads && wget "https://repo.anaconda.com/miniconda/${CONDA}"
+chmod +x ~/Downloads/${CONDA}
 # start | accept | location | init
-printf '\nyes\n\nyes\n' | sh ~/Downloads/CONDA
-bash
+printf '\nyes\n\nyes\n' | sh ~/Downloads/${CONDA}
+source ~/.bashrc
 
 # process to install fish-shell
 printf '\n' | conda install -c conda-forge fish
