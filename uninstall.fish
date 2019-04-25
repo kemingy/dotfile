9911bd3d84665e82a3eeccbabce9df979468a6b0
Keming <kemingy94@email.com>
2019-04-25 10:51:29 +0800

    uninstall -r -f

--- uninstall.fish
+++ uninstall.fish
@@ -6,6 +6,7 @@ printf 'y\n' | rustup self uninstall
 
 # fish
 rm $HOME/.config/fish/config.fish
-rm $HOME/.local/share/omf
+rm -rf $HOME/.local/share/omf
+rm -rf $HOME/.local/share/fish
 printf 'y\n' | omf destroy
 conda remove -y fish
