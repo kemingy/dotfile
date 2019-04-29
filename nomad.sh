39b4bfbc190b4e28d6630a9ab3c8af921aa0c0c6
Keming <keming.yang@shopee.com>
2019-04-29 15:34:43 +0800

    add nomad setup

---
+++ nomad.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+VAGRANT_VERSION="2.2.4"
+
+# virtualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb https://download.oracle.com/virtualbox/debian xenial contrib"
+sudo apt-get update
+sudo apt-get install virtualbox-6.0
+
+# vagrant
+wget -O ${HOME}/Downloads/ "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb"
+sudo dpkg -i ${HOME}/Downloads/vagrant_${VAGRANT_VERSION}_x86_64.deb
+
+# nomad
+mkdir -p ${HOME}/Code/Nomad
+wget -O ${HOME}/Code/Nomad/Vagrantfile "https://raw.githubusercontent.com/hashicorp/nomad/master/demo/vagrant/Vagrantfile"
+cd ${HOME}/Code/Nomad && vagrant up
