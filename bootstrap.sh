853e4f047e0f79611745c2e62417212e96d93193
keming <kemingy94@gmail.com>
2019-08-30 11:55:34 +0800

    update boostrap file, fix #4 #5

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,8 @@
 #!/bin/bash
-set -e
 
-CONDA="Miniconda3-latest-Linux-x86_64.sh"
-
-# conda
-mkdir -p ${HOME}/Downloads
-[ -f "${HOME}/Downloads/${CONDA}" ] || wget -O ${HOME}/Downloads/${CONDA} "https://repo.anaconda.com/miniconda/${CONDA}"
-sh ${HOME}/Downloads/${CONDA} -b -u
-${HOME}/miniconda3/bin/conda init
-. ${HOME}/.bashrc
-pip install --upgrade https://github.com/jakubroztocil/httpie/archive/master.tar.gz
-
-# vimrc
-git clone --depth=1 https://github.com/amix/vimrc.git ${HOME}/.vim_runtime
-sh ${HOME}/.vim_runtime/install_awesome_vimrc.sh
-cat vim/.vimrc >> ${HOME}/.vim_runtime/my_configs.vim
-
-# git
-git config --global core.excludesfile ~/.gitignore_global
-cp .gitignore_global ~/.gitignore_global
-
-# tmux conf
-cp .tmux.conf ${HOME}/
-
-# Rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-. ${HOME}/.cargo/env
-cargo install lsd --force
-cargo install bat --force
-cargo install xsv --force
-
-# fish-shell
-conda install -y -c conda-forge fish
-mkdir -p ${HOME}/.config/fish/
-cat config.fish >> ${HOME}/.config/fish/config.fish
-curl -L https://get.oh-my.fish | fish
-omf install cbjohnson
+for cmd in py.sh vim.sh git.sh tmux.sh rust.sh fish.sh
+do
+    sh $cmd
+done
 
 echo "Enjoy :-)"
