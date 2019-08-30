8adedffa6e5f63568b65815f2107958d3488d48f
keming <kemingy94@gmail.com>
2019-08-30 14:25:19 +0800

    install from deb

--- rust.sh
+++ rust.sh
@@ -4,6 +4,19 @@ set -e
 # Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . ${HOME}/.cargo/env
-cargo install lsd --force
-cargo install bat --force
-cargo install xsv --force
+
+# lsd
+LSD_VERSION="0.16.0"
+LSD="lsd_${LSD_VERSION}_amd64.deb"
+
+[ -f "${HOME}/Downloads/${LSD}" ] || wget -O ${HOME}/Downloads/${LSD} "https://github.com/Peltoche/lsd/releases/download/${LSD_VERSION}/${LSD}"
+
+sudo dpkg -i ${HOME}/Downloads/${LSD}
+
+
+# bat
+BAT_VERSION="0.11.0"
+BAT="bat_${BAT_VERSION}_amd64.deb"
+
+[ -f "${HOME}/Downloads/${BAT}" ] || wget -O ${HOME}/Downloads/${BAT} "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/${BAT}"
+sudo dpkg -i ${HOME}/Downloads/${BAT}
