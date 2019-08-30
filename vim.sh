9385e2dec4246547027d1ebc8549a0e90d6910cc
keming <kemingy94@gmail.com>
2019-08-30 14:43:31 +0800

    cp instead of cat >>

--- vim.sh
+++ vim.sh
@@ -4,6 +4,6 @@ set -e
 # vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ${HOME}/.vim_runtime
 sh ${HOME}/.vim_runtime/install_awesome_vimrc.sh
-cat vim/.vimrc >> ${HOME}/.vim_runtime/my_configs.vim
+cp vim/.vimrc ${HOME}/.vim_runtime/my_configs.vim
 mkdir -p ${HOME}/.vim/colors/
 curl https://raw.githubusercontent.com/rakr/vim-one/master/colors/one.vim > ${HOME}/.vim/colors/one.vim
