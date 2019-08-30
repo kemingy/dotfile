3ea52148ff8c51c74e4b0cdd6d4833a82b46a567
keming <kemingy94@gmail.com>
2019-08-30 14:43:31 +0800

    cp instead of cat >>

--- fish.sh
+++ fish.sh
@@ -4,6 +4,6 @@ set -e
 # fish-shell
 ${HOME}/miniconda3/bin/conda install -y -c conda-forge fish
 mkdir -p ${HOME}/.config/fish/
-cat config.fish >> ${HOME}/.config/fish/config.fish
+cp config.fish ${HOME}/.config/fish/config.fish
 curl -L https://get.oh-my.fish | fish
 omf install cbjohnson
