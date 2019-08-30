3bef5f5aa0aa7c519fbff2d0fa4d6e0d81f2727b
keming <kemingy94@gmail.com>
2019-08-30 14:25:06 +0800

    use abs path

--- fish.sh
+++ fish.sh
@@ -2,8 +2,7 @@
 set -e
 
 # fish-shell
-. ${HOME}/.bashrc
-conda install -y -c conda-forge fish
+${HOME}/miniconda3/bin/conda install -y -c conda-forge fish
 mkdir -p ${HOME}/.config/fish/
 cat config.fish >> ${HOME}/.config/fish/config.fish
 curl -L https://get.oh-my.fish | fish
