b2e2e9152ef4ec590708a60b7e4cd50c2065e856
keming <kemingy94@gmail.com>
2019-12-04 15:10:31 +0800

    fix: if unzip is not found

--- rust.sh
+++ rust.sh
@@ -5,14 +5,6 @@ set -e
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . ${HOME}/.cargo/env
 
-# exa
-EXA_VERSION="0.9.0"
-EXA="exa-linux-x86_64-${EXA_VERSION}.zip"
-
-[ -f "${HOME}/Downloads/${EXA}" ] || wget -O ${HOME}/Downloads/${EXA} "https://github.com/ogham/exa/releases/download/v${EXA_VERSION}/${EXA}"
-unzip "${HOME}/Downloads/${EXA}" -d "${HOME}/Downloads/"
-sudo cp "${HOME}/Downloads/exa-linux-x86_64" /usr/local/bin/exa
-
 # bat
 BAT_VERSION="0.11.0"
 BAT="bat_${BAT_VERSION}_amd64.deb"
@@ -26,3 +18,14 @@ RG="ripgrep_${RG_VERSION}_amd64.deb"
 
 [ -f "${HOME}/Downloads/${RG}" ] || wget -O ${HOME}/Downloads/${RG} "https://github.com/BurntSushi/ripgrep/releases/download/${RG_VERSION}/${RG}"
 sudo dpkg -i ${HOME}/Downloads/${RG}
+
+# exa
+EXA_VERSION="0.9.0"
+EXA="exa-linux-x86_64-${EXA_VERSION}.zip"
+
+[ -f "${HOME}/Downloads/${EXA}" ] || wget -O ${HOME}/Downloads/${EXA} "https://github.com/ogham/exa/releases/download/v${EXA_VERSION}/${EXA}"
+if [ ! -d "$DIR" ]; then
+    sudo apt install zip -y
+fi
+unzip "${HOME}/Downloads/${EXA}" -d "${HOME}/Downloads/"
+sudo cp "${HOME}/Downloads/exa-linux-x86_64" /usr/local/bin/exa
