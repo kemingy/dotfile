02e8b5c0082a0e78262dbf00e58d57b993814b21
keming <kemingy94@gmail.com>
2020-04-05 19:03:27 +0800

    fix fish version

--- fish.sh
+++ fish.sh
@@ -2,7 +2,7 @@
 set -e
 
 # fish-shell
-${HOME}/miniconda3/bin/conda install -y -c conda-forge fish
+${HOME}/miniconda3/bin/conda install -y -c conda-forge fish=3.0
 mkdir -p ${HOME}/.config/fish/
 cp config.fish ${HOME}/.config/fish/config.fish
 curl -L https://get.oh-my.fish | ${HOME}/miniconda3/bin/fish
