e4c584139ebb35ca8cb9b57471ca273b577cd508
keming <kemingy94@gmail.com>
2020-04-05 17:49:34 +0800

    fix bash

--- rust.sh
+++ rust.sh
@@ -10,7 +10,7 @@ BAT_VERSION="0.11.0"
 BAT="bat_${BAT_VERSION}_amd64.deb"
 
 [ -f "${HOME}/Downloads/${BAT}" ] || wget -O ${HOME}/Downloads/${BAT} "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/${BAT}"
-if [[ $EUID -ne 0 ]]; then
+if [ $EUID -ne 0 ]; then
     sudo dpkg -i ${HOME}/Downloads/${BAT}
 else
     dpkg -i ${HOME}/Downloads/${BAT}
@@ -21,7 +21,7 @@ RG_VERSION="11.0.2"
 RG="ripgrep_${RG_VERSION}_amd64.deb"
 
 [ -f "${HOME}/Downloads/${RG}" ] || wget -O ${HOME}/Downloads/${RG} "https://github.com/BurntSushi/ripgrep/releases/download/${RG_VERSION}/${RG}"
-if [[ $EUID -ne 0 ]]; then
+if [ $EUID -ne 0 ]; then
     sudo dpkg -i ${HOME}/Downloads/${RG}
 else
     dpkg -i ${HOME}/Downloads/${RG}
@@ -33,10 +33,14 @@ EXA="exa-linux-x86_64-${EXA_VERSION}.zip"
 
 [ -f "${HOME}/Downloads/${EXA}" ] || wget -O ${HOME}/Downloads/${EXA} "https://github.com/ogham/exa/releases/download/v${EXA_VERSION}/${EXA}"
 if [ ! -d "$DIR" ]; then
-    sudo apt install zip -y
+    if [ $EUID -ne 0]; then
+        sudo apt install zip -y
+    else
+        apt install zip -y
+    fi
 fi
 unzip "${HOME}/Downloads/${EXA}" -d "${HOME}/Downloads/"
-if [[ $EUID -ne 0 ]]; then
+if [ $EUID -ne 0 ]; then
     sudo cp "${HOME}/Downloads/exa-linux-x86_64" /usr/local/bin/exa
 else
     cp "${HOME}/Downloads/exa-linux-x86_64" /usr/local/bin/exa
