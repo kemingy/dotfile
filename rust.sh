a2cf18951b539d99fc331869ddd985d778c98ae1
keming <kemingy94@gmail.com>
2021-02-22 10:43:10 +0800

    add delta

--- rust.sh
+++ rust.sh
@@ -46,5 +46,15 @@ else
     cp "${HOME}/Downloads/exa-linux-x86_64" /usr/local/bin/exa
 fi
 
+# delta
+DELTA_VERSION="0.6.0"
+DELTA="git-delta_0.6.0_amd64.deb"
+[ -f "${HOME}/Downloads/${DELTA}" ] || wget -O ${HOME}/Downloads/${DELTA} "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/${DELTA}"
+if [ $EUID -ne 0 ]; then
+    sudo dpkg -i ${HOME}/Downloads/${DELTA}
+else
+    dpkg -i ${HOME}/Downloads/${DELTA}
+fi
+
 # starship
 bash -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes --platform unknown-linux-musl
