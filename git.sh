38a3e7ce96d2af5e6db4353989cb28d728c7b966
Keming <kemingy94@gmail.com>
2021-10-01 12:15:16 +0800

    add git adog

--- git.sh
+++ git.sh
@@ -2,5 +2,6 @@
 set -e
 
 # git
+git config --global alias.adog "log --all --decorate --oneline --graph"
 git config --global core.excludesfile ~/.gitignore_global
 cp .gitignore_global ~/.gitignore_global
