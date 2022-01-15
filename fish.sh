18e9098f4decd688c4ade9a950c02a04280cd8bf
Keming <kemingy94@gmail.com>
2022-01-15 18:48:37 +0800

    fix fish

--- fish.sh
+++ fish.sh
@@ -2,7 +2,7 @@
 set -e
 
 # fish-shell
-${HOME}/miniconda3/bin/conda install -y -c conda-forge
+${HOME}/miniconda3/bin/conda install -y -c conda-forge fish
 mkdir -p ${HOME}/.config/fish/
 cp config.fish ${HOME}/.config/fish/config.fish
 
