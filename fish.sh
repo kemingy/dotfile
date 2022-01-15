9f31c3c0d4e040bc21b15288e87b989e5ee29a7c
Keming <kemingy94@gmail.com>
2022-01-15 16:52:31 +0800

    rm oh my fish

--- fish.sh
+++ fish.sh
@@ -2,8 +2,7 @@
 set -e
 
 # fish-shell
-${HOME}/miniconda3/bin/conda install -y -c conda-forge fish=3.0
+${HOME}/miniconda3/bin/conda install -y -c conda-forge
 mkdir -p ${HOME}/.config/fish/
 cp config.fish ${HOME}/.config/fish/config.fish
-curl -L https://get.oh-my.fish | ${HOME}/miniconda3/bin/fish
-# omf install cbjohnson
+
