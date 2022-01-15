7565f9106f5dffe3fa650997e583902d291a3f59
Keming <kemingy94@gmail.com>
2022-01-15 18:39:43 +0800

    update delta

--- rust.sh
+++ rust.sh
@@ -6,12 +6,9 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 . ${HOME}/.cargo/env
 
 # delta
-DELTA_VERSION="0.6.0"
-DELTA="git-delta_0.6.0_amd64.deb"
+DELTA_VERSION="0.11.3"
+DELTA="delta-${DELTA_VERSION}-x86_64-unknown-linux-musl.tar.gz"
 [ -f "${HOME}/Downloads/${DELTA}" ] || wget -O ${HOME}/Downloads/${DELTA} "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/${DELTA}"
-if [ $EUID -ne 0 ]; then
-    sudo dpkg -i ${HOME}/Downloads/${DELTA}
-else
-    dpkg -i ${HOME}/Downloads/${DELTA}
-fi
+tar zxf ${HOME}/Downloads/${DELTA} -C ${HOME}/Downloads/ --strip-components 1
+cp ${HOME}/Downloads/delta ${HOME}/miniconda3/bin/
 
