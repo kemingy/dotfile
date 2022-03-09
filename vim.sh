504a506488d96d997fbb5bf60e2bf44203e4113a
Keming <kemingy94@gmail.com>
2022-03-09 22:02:00 +0800

    fix nvim path

--- vim.sh
+++ vim.sh
@@ -4,8 +4,8 @@ set -e
 # Neovim
 VERSION="v0.6.1"
 [ -f "${HOME}/Downloads/nvim-${VERSION}.tar.gz" ] || wget -O ${HOME}/Downloads/nvim-${VERSION}.tar.gz "https://github.com/neovim/neovim/releases/download/${VERSION}/nvim-linux64.tar.gz"
-tar zxf "${HOME}/Downloads/nvim-${VERSION}.tar.gz" -C "${HOME}/Downloads/nvim" --strip-components 1
-echo "export PATH=${HOME}/Downloads/nvim/bin:\$PATH" >> "${HOME}/.bashrc"
+tar zxf "${HOME}/Downloads/nvim-${VERSION}.tar.gz" -C "${HOME}/Downloads/"
+echo "export PATH=${HOME}/Downloads/nvim-linux64/bin:\$PATH" >> "${HOME}/.bashrc"
 
 # vim plugin
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
