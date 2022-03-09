851c4b45478af0cf5ef7f40b13c12c874e2a9a7d
Keming <kemingy94@gmail.com>
2022-03-09 21:46:57 +0800

    change to neovim

--- vim.sh
+++ vim.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-# vimrc
-git clone --depth=1 https://github.com/amix/vimrc.git ${HOME}/.vim_runtime
-sh ${HOME}/.vim_runtime/install_awesome_vimrc.sh
-cp vim/.vimrc ${HOME}/.vim_runtime/my_configs.vim
-mkdir -p ${HOME}/.vim/colors/
-curl https://raw.githubusercontent.com/rakr/vim-one/master/colors/one.vim > ${HOME}/.vim/colors/one.vim
+# Neovim
+VERSION="v0.6.1"
+[ -f "${HOME}/Downloads/nvim-${VERSION}.tar.gz" ] || wget -O ${HOME}/Downloads/nvim-${VERSION}.tar.gz "https://github.com/neovim/neovim/releases/download/${VERSION}/nvim-linux64.tar.gz"
+tar zxf "${HOME}/Downloads/nvim-${VERSION}.tar.gz" -C "${HOME}/Downloads/nvim" --strip-components 1
+echo "export PATH=${HOME}/Downloads/nvim/bin:\$PATH" >> "${HOME}/.bashrc"
+
+# vim plugin
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
