7a4ec3ba71ec82d1e6cc51579daadfc918937fb4
Keming <kemingy94@gmail.com>
2022-05-11 15:13:51 +0800

    fix nvim version and install plugin automaticall

--- vim.sh
+++ vim.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Neovim
-VERSION="v0.6.1"
+VERSION="v0.7.0"
 [ -f "${HOME}/Downloads/nvim-${VERSION}.tar.gz" ] || wget -O ${HOME}/Downloads/nvim-${VERSION}.tar.gz "https://github.com/neovim/neovim/releases/download/${VERSION}/nvim-linux64.tar.gz"
 tar zxf "${HOME}/Downloads/nvim-${VERSION}.tar.gz" -C "${HOME}/Downloads/"
 echo "export PATH=${HOME}/Downloads/nvim-linux64/bin:\$PATH" >> "${HOME}/.bashrc"
@@ -13,3 +13,7 @@ cp ./init.vim ${HOME}/.config/nvim
 # vim plugin
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# :PlugInstall
+${HOME}/Downloads/nvim-linux64/bin/nvim +PlugInstall +qall
+
