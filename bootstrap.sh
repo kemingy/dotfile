9d888cfb2a725cdcd54e26ccd67800d2aa7b7e92
Keming <kemingy94@gmail.com>
2022-12-18 22:57:54 +0800

    fix nvim install script
    Signed-off-by: Keming <kemingy94@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cmd in base.sh py.sh vim.sh git.sh tmux.sh rust.sh fish.sh
+for cmd in base.sh py.sh vim.sh git.sh rust.sh fish.sh
 do
     bash $cmd
 done
--- vim.sh
+++ vim.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # Neovim
 VERSION="nightly"
 [ -f "$/tmp/nvim-${VERSION}.tar.gz" ] || wget -O /tmp/nvim-${VERSION}.tar.gz "https://github.com/neovim/neovim/releases/download/${VERSION}/nvim-linux64.tar.gz"
-mkdir -p .nvim
-tar zxf --strip-components=1 "/tmp/nvim-${VERSION}.tar.gz" -C "${HOME}/.nvim"
+mkdir -p ${HOME}/.nvim
+tar zxf "/tmp/nvim-${VERSION}.tar.gz" -C "${HOME}/.nvim" --strip-components=1
 echo "export PATH=${HOME}/.nvim/bin:\$PATH" >> "${HOME}/.bashrc"
 
 mkdir -p ${HOME}/.config/nvim
