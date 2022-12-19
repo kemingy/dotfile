e05797817b60b98877d34d76c7880898023c33a3
Keming <kemingy94@gmail.com>
2022-12-19 10:11:48 +0800

    config nvim indent
    Signed-off-by: Keming <kemingy94@gmail.com>

--- nvim.lua
+++ nvim.lua
@@ -198,9 +198,15 @@ require('Comment').setup()
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
 require('indent_blankline').setup {
   char = '┊',
-  show_trailing_blankline_indent = false,
+  show_current_context = true,
+  show_current_context_start = true,
+  show_end_of_line = true,
+  show_trailing_blankline_indent = true,
 }
 
 -- Gitsigns
