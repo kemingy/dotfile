3545e9c0cfeefa3998277f3fcbe98072242360a5
Keming <kemingy94@gmail.com>
2022-12-20 22:31:24 +0800

    update difft
    Signed-off-by: Keming <kemingy94@gmail.com>

--- rust.sh
+++ rust.sh
@@ -6,7 +6,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . ${HOME}/.cargo/env
 
 # difftastic
-DIFF_VERSION="0.31.0"
+DIFF_VERSION="0.39.0"
 DIFF="difft-x86_64-unknown-linux-gnu.tar.gz"
 [ -f "${HOME}/Downloads/${DIFF}" ] || wget -O ${HOME}/Downloads/${DIFF} "https://github.com/Wilfred/difftastic/releases/download/${DIFF_VERSION}/${DIFF}"
 tar zxf ${HOME}/Downloads/${DIFF} -C ${HOME}/Downloads/
