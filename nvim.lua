9de70d54208037407dc059610d91e5caa065062b
Keming <kemingy94@gmail.com>
2022-12-23 14:44:12 +0800

    show relative path
    Signed-off-by: Keming <kemingy94@gmail.com>

--- nvim.lua
+++ nvim.lua
@@ -104,7 +104,7 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 -- See `:help vim.o`
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 vim.o.incsearch = true
 
 -- Make line numbers default
@@ -207,6 +207,16 @@ require('lualine').setup {
     component_separators = '|',
     section_separators = '',
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        newfile_status = true,
+        path = 1,
+      },
+    },
+  },
 }
 
 -- Enable Comment.nvim
@@ -238,6 +248,7 @@ require('gitsigns').setup {
     topdelete = { text = '‾' },
     changedelete = { text = '~' },
   },
+  current_line_blame = true,
 }
 
 -- [[ Configure Telescope ]]
@@ -273,6 +284,7 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>re', require('telescope.builtin').resume, { desc = '[RE]sume last search buffer' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
