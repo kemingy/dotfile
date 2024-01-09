16509c6afd3d9e25d7116022203cdc84c51702c1
Keming <kemingy94@gmail.com>
2024-01-09 12:14:33 +0800

    fix conda path
    Signed-off-by: Keming <kemingy94@gmail.com>

--- fish.sh
+++ fish.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 # fish-shell
-${HOME}/miniconda3/bin/conda install -y -c conda-forge fish
+/opt/conda/bin/conda install -y -c conda-forge fish
 mkdir -p ${HOME}/.config/fish/
 cp config.fish ${HOME}/.config/fish/config.fish
--- rust.sh
+++ rust.sh
@@ -10,4 +10,5 @@ DIFF_VERSION="0.39.0"
 DIFF="difft-x86_64-unknown-linux-gnu.tar.gz"
 [ -f "/tmp/${DIFF}" ] || wget -O /tmp/${DIFF} "https://github.com/Wilfred/difftastic/releases/download/${DIFF_VERSION}/${DIFF}"
 tar zxf /tmp/${DIFF} -C /tmp/
-cp /tmp/difft ${HOME}/.local/bin/
+mkdir -p ${HOME}/.local/bin
+cp /tmp/difft ${HOME}/.local/bin/difft
