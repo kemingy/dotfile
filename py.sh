6bb0e71d8a67af16daf0b007f279f5b77fa10a0a
Keming <kemingy94@gmail.com>
2024-01-11 17:15:20 +0800

    add typos to conda
    Signed-off-by: Keming <kemingy94@gmail.com>

--- py.sh
+++ py.sh
@@ -11,6 +11,6 @@ sh /tmp/${CONDA} -b -u -p /opt/conda
 /opt/conda/bin/conda init
 
 # related libraries
-/opt/conda/bin/conda install -y -c conda-forge bat ripgrep exa fd-find starship gh
+/opt/conda/bin/conda install -y -c conda-forge bat ripgrep exa fd-find starship gh typos
 /opt/conda/bin/pip install httpie ruff black ipython
 mkdir -p ${HOME}/.config
