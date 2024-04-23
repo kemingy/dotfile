725623bdb924e91ae91d647ce0ff4027af2e7cd8
Keming <kemingy94@gmail.com>
2024-04-23 10:36:33 +0800

    move difft to git and update version, install ncurses for fish-shell
    Signed-off-by: Keming <kemingy94@gmail.com>

--- git.sh
+++ git.sh
@@ -5,3 +5,11 @@ set -euo pipefail
 git config --global alias.adog "log --all --decorate --oneline --graph"
 git config --global core.excludesfile ~/.gitignore_global
 cp .gitignore_global ~/.gitignore_global
+
+# difftastic
+DIFF_VERSION="0.57.0"
+DIFF="difft-x86_64-unknown-linux-gnu.tar.gz"
+[ -f "/tmp/${DIFF}" ] || wget -O /tmp/${DIFF} "https://github.com/Wilfred/difftastic/releases/download/${DIFF_VERSION}/${DIFF}"
+tar zxf /tmp/${DIFF} -C /tmp/
+mkdir -p ${HOME}/.local/bin
+cp /tmp/difft ${HOME}/.local/bin/difft
--- rust.sh
+++ rust.sh
@@ -4,11 +4,3 @@ set -euo pipefail
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . ${HOME}/.cargo/env
-
-# difftastic
-DIFF_VERSION="0.39.0"
-DIFF="difft-x86_64-unknown-linux-gnu.tar.gz"
-[ -f "/tmp/${DIFF}" ] || wget -O /tmp/${DIFF} "https://github.com/Wilfred/difftastic/releases/download/${DIFF_VERSION}/${DIFF}"
-tar zxf /tmp/${DIFF} -C /tmp/
-mkdir -p ${HOME}/.local/bin
-cp /tmp/difft ${HOME}/.local/bin/difft
