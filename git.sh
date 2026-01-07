a71950e83adc21cb49c518f8bc44b72bed40dc3e
Keming <kemingy94@gmail.com>
2026-01-07 16:16:50 +0800

    add devcontainer to be used as a template in coder
    Signed-off-by: Keming <kemingy94@gmail.com>

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@ cp .gitconfig ${HOME}/.gitconfig
 cp .gitignore_global ~/.gitignore_global
 
 # difftastic
-DIFF_VERSION="0.57.0"
+DIFF_VERSION="0.67.0"
 DIFF="difft-x86_64-unknown-linux-gnu.tar.gz"
 [ -f "/tmp/${DIFF}" ] || wget -O /tmp/${DIFF} "https://github.com/Wilfred/difftastic/releases/download/${DIFF_VERSION}/${DIFF}"
 tar zxf /tmp/${DIFF} -C /tmp/
