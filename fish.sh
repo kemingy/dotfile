1175ce2908043c51e39d1adf891c61c06c0c6fcb
Keming <kemingy94@gmail.com>
2026-01-07 16:23:24 +0800

    fix: replace conda with pixi
    Signed-off-by: Keming <kemingy94@gmail.com>

--- fish.sh
+++ fish.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 # fish-shell
-/opt/conda/bin/conda install -y -c conda-forge fish
+# $HOME/.pixi/bin/pixi install -y -c conda-forge fish
 mkdir -p ${HOME}/.config/fish/
 cp config.fish ${HOME}/.config/fish/config.fish
--- uninstall.sh
+++ uninstall.sh
@@ -7,4 +7,4 @@ printf 'y\n' | rustup self uninstall
 # fish
 rm $HOME/.config/fish/config.fish
 rm -rf $HOME/.local/share/fish
-conda remove -y fish
+pixi remove -y fish
